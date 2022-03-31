9a61bd0572becb0a886697a8da7c150bc450292f
emybarr <eybarreiro.2a@gmail.com>
2022-03-31 19:29:17 -0300

    opcion 2

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
 
 #define N 20 
 
@@ -8,12 +8,12 @@ int main(){
     
     int i;
     double vt[N];
-    srand(time(NULL));
+ 
     
 
     for(i = 0 ; i < N ; i++){
         vt[i] = 1 + rand()% 100 ;
-        printf ("%d  " , vt[i]);
+        printf ("%.1f  " , vt[i]);
        
     }
 
